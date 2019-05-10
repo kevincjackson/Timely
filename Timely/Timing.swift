d436b2c2adaabc2aabd5a703da469d638a2ce74a
Kevin Jackson <kj31428@gmail.com>
2019-05-10 13:51:24 -0700

    Initial commit.

---
+++ Timely/Timing.swift
@@ -0,0 +1,15 @@
+//
+//  Timing.swift
+//  Timely
+//
+//  Created by Kevin Jackson on 5/10/19.
+//  Copyright © 2019 Kevin Jackson. All rights reserved.
+//
+
+import UIKit
+
+struct Timing {
+    var name: String
+    var color: UIColor
+    var image: UIImage
+}
---
+++ Timely/TimingEntry.swift
@@ -0,0 +1,15 @@
+//
+//  TimingEntry.swift
+//  Timely
+//
+//  Created by Kevin Jackson on 5/10/19.
+//  Copyright © 2019 Kevin Jackson. All rights reserved.
+//
+
+import Foundation
+
+struct TimingEntry {
+    var timing: Timing
+    var date: Date
+}
+
